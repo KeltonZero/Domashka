986f230b454e82293d1fbce037ce49629e0765c5
KeltonZero <fedyunin_a@mail.ua>
2021-02-26 20:56:09 +0300

    PW1Ex1

---
+++ PW1Ex1/EX1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//Вывести на экран Первую букву имени из символов "*" (звездочка)
+	cout << "   *   " << endl;
+	cout << "  * *  " << endl;
+	cout << " ***** " << endl;
+	cout << "*     *" << endl;
+
+
+
+
+
+
+	return 0;
+}
